a92d1b6ccef8076f35aa82cf4c9d15f79d2c5cd1
OneView <kougaragency@gmail.com>
2026-03-19 00:55:38 -0300

    feat: logo más grande en navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
             alt="Feis Digital"
             width={120}
             height={40}
-            className="h-10 w-auto object-contain"
+            className="h-14 w-auto object-contain"
             priority
           />
         </a>
